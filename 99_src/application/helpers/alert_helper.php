58a081bc1c255097bc9b9656df4ce54b217ba60c
starseat <starseat3@gmail.com>
2021-04-08 17:20:09 +0900

    [CI] alert helper 추가

---
+++ 99_src/application/helpers/alert_helper.php
@@ -0,0 +1,31 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+function alert($msg = '', $url = '') {
+	$CI =& get_instance();
+	echo '<meta http-equiv="content-type" content="text/html; charset="' . $CI->config->item('charset') . '">';
+	echo '<script type="text/javascript"> alert("' . $msg . '"); location.replace("' . $url . '"); </script>';
+	exit;
+}
+
+function alert_close($msg) {
+	$CI = &get_instance();
+	echo '<meta http-equiv="content-type" content="text/html; charset="' . $CI->config->item('charset') . '">';
+	echo '<script type="text/javascript"> alert("' . $msg . '"); window.close(); </script>';
+	exit;
+}
+
+function alert_only($msg, $exit=true) {
+	$CI = &get_instance();
+	echo '<meta http-equiv="content-type" content="text/html; charset="' . $CI->config->item('charset') . '">';
+	echo '<script type="text/javascript"> alert("' . $msg . '"); </script>';
+
+	if($exit) {
+		exit;
+	}	
+}
+
+function replace($url = '/') {
+	echo '<script type="text/javascript"> location.replace("' . $url . '"); </script>';
+	exit;
+}

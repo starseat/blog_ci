722ae1e2632ed5e97554d8a25fe42e34073b37db
starseat <starseat3@gmail.com>
2021-05-17 18:32:57 +0900

    [WEB] API 테스트

--- 99_src/application/controllers/Api/Upload.php
+++ 99_src/application/controllers/Api/Upload.php
@@ -1,8 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-// class Upload extends Base_Api_Controller {
-class Upload extends Base_Controller {
+class Upload extends Base_ApiController {
+// class Upload extends Base_Controller {
 	public function __construct() {
         parent::__construct();
 	}

fc9a3094e97acf3e7780a11fad3a191201233a8b
starseat <starseat3@gmail.com>
2021-05-25 10:33:09 +0900

    [WEB] 아이콘 및 메타데이터 적용

--- 99_src/application/views/fragments/head.php
+++ 99_src/application/views/fragments/head.php
@@ -6,9 +6,20 @@
 	<!--- basic page needs
     ================================================== -->
 	<meta charset="utf-8">
-	<title>Typerite</title>
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<title>Scribble - jw's blog.</title>
+	<meta name="description" content="개인 블로그 입니다.">
+	<meta name="author" content="starseat">
+
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="Scribble - jw's blog.">
+	<meta property="og:description" content="개인 블로그 입니다.">
+	<meta property="og:url" content="http://starseat.net/">
+	<meta property="og:image" content="/public/imgs/og-image.png" />
+
+	<!-- meta http-equiv
+    ================================================== -->
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
 	<!-- mobile specific metas
     ================================================== -->
@@ -26,12 +37,11 @@
 
 	<!-- favicons
     ================================================== -->
-	<!-- 
-	<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-	<link rel="icon" type="/public/template/image/png" sizes="32x32" href="favicon-32x32.png">
-	<link rel="icon" type="/public/template/image/png" sizes="16x16" href="favicon-16x16.png">
-	<link rel="manifest" href="site.webmanifest">
-	-->
+	<link rel="apple-touch-icon" sizes="180x180" href="/public/imgs/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/public/imgs/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/public/imgs/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/public/imgs/favicon/site.webmanifest">
+
 </head>
 
 <body>
